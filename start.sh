31f6d8af40b6eccbd15dc2e397f5cc52e584831a
onejati <omidnejati.it@gmail.com>
2016-11-04 16:03:19 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
 sleep 3
-kafka-config.sh
+
+/kafka/kafka-config.sh
+
 sleep 1
+
 bin/kafka-server-start.sh config/server.properties
+
 sleep 1
-tail -F /logs/kafkaServer.out
+
+tail -f logs/server.log 
